a3aa0ea9d63cd8c7c4bcb9de3f5c779c7405ad51
hawawawawawawawa <23-1621@sonline20.sen.go.kr>
2025-07-15 09:47:12 +0900

    Update first1.py

--- first1.py
+++ first1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
 name = st.text_input('이름을 입력해주세요 : ')
-menu = st.selectbox('좋아하는 행동을 선택해주세요:', ['등교','하교'])
+menu = st.selectbox('❤좋아하는 행동을 선택해주세요:', ['등교','하교'])
 if st.button('인사말 생성') : 
-  st.write(name+'님! 당신이 좋아하는 행동은 '+menu+'이군요?! 저도 좋아요!!')
+  st.write(name+'님! 당신이 좋아하는 행동은 '+menu+'이군요?! 저도 좋아요!!🐱‍👤')
