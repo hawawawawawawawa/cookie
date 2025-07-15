b664d541968dc3ea34546e15413f56d888eeb814
hawawawawawawawa <23-1621@sonline20.sen.go.kr>
2025-07-15 10:19:31 +0900

    Update mathquiz.py

--- mathquiz.py
+++ mathquiz.py
@@ -1,14 +1,16 @@
 import streamlit as st
-st.latex(r"""
-\lim_{x \to \infty}\frac{x^2+2x+a}{x^2}=a , a = ?
-""")
-answer = st.selectbox(
-    "정답을 입력하세요",
-    ["1", "2", "3", "4"]
-)
-if st.button('정답 확인') :
-    if answer == "1":
+if "page" not in st.session_state:
+    st.session_state.page = 1
+    st.latex(r"""
+    \lim_{x \to \infty}\frac{x^2+2x+a}{x^2}=a , a = ?
+    """)
+    answer = st.selectbox(
+        "정답을 입력하세요",
+        ["1", "2", "3", "4"]
+    )
+    if st.button('정답 확인') :
+        if answer == "1":
             st.title("정답")
             st.write("정답입니다.")
-    else :
-         st.write("다시 한 번 생각해보세요")
+        else :
+            st.write("다시 한 번 생각해보세요")
