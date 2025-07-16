9cf86387a44b2d9f306b0364aa2289181cb1d44a
hawawawawawawawa <23-1621@sonline20.sen.go.kr>
2025-07-17 08:29:40 +0900

    Delete mathquiz.py

--- mathquiz.py
+++ mathquiz.py
@@ -1,18 +1,1 @@
-import streamlit as st
-if "page" not in st.session_state:
-    st.session_state.page = 1
-    st.latex(r"""
-    \lim_{x \to \infty}\frac{x^2+2x+a}{x^2}=a , a = ?
-    """)
-    answer = st.selectbox(
-        "정답을 입력하세요",
-        ["1", "2", "3", "4"]
-    )
-    if st.button('정답 확인') :
-        if answer == "1":
-            st.session_state.page = 2
-            if st.session_state.page == 2:
-                st.title("정답")
-                st.write("정답입니다.")
-        else :
-            st.write("다시 한 번 생각해보세요")
+
